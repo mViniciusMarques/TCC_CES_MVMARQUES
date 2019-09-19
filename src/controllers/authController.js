9c23627ab6a0bf4ebb5e7582927c65b7fb0fd8a4
Marcus Vinicius De Oliveira Marques <mvmarques2@stefanini.com>
2019-09-19 11:12:46 -0300

    commit estrutural node-bridge

---
+++ src/controllers/authController.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const User = require('../model/users.js')
+
+const router = express.Router()
+
+router.post('/register', async(req, res) => {
+    try {
+        if(await User.findOne({ email })) {
+            return res.status(400).send({ error: 'User already exists'});
+        }
+
+        const user = await  User.create(req.body)
+        
+        user.password = undefined;
+
+        return res.send(user)
+    } catch(err) {
+        return res.status(400).send({ error: 'Registration failed'})
+    }
+})
+
+module.exports = app => app.use('/auth', router);
---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('../database');
+
+mongoose.connect('mongodb://localhost/noderest', {useMongoClient: true});
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
